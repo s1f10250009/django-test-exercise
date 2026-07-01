499fa8cc1fc980c2e2b0bdf33fdcafd040fa8b3a
1F10250009 <s1f102500090@iniad.org>
2026-07-01 13:57:02 +0900

    バグ修正

--- todo/models.py
+++ todo/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Task(models.Model):
-    title = models.CharField(max_length = 100)
+    title = models.CharField(max_length=100)
     completed = models.BooleanField(default=False)
     posted_at = models.DateTimeField(default=timezone.now)
-    due_at = models.DateTimeField(null=True,blank=True)
+    due_at = models.DateTimeField(null=True, blank=True)
 
     def is_overdue(self,dt):
         if self.due_at is None:
--- todo/tests.py
+++ todo/tests.py
@@ -3,10 +3,11 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
-# Create your tests here.
+
 class SampleTestCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1 + 2, 3)
+
 
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
@@ -53,6 +54,7 @@ class TaskModelTestCase(TestCase):
 
         self.assertFalse(task.is_overdue(current))
 
+
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
         client = Client()
@@ -96,3 +98,4 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(response.context['tasks'][0], task1)
         self.assertEqual(response.context['tasks'][1], task2)
+        
